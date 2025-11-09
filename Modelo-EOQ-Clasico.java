b48bd0d571ec427b3a9f34b1c7da940482e775f8
Carlos Manuel <d24390246@huauchinango.tecnm.mx>
2025-11-09 16:58:35 +0000

    avance del codigo

--- Modelo-EOQ-Clasico.java
+++ Modelo-EOQ-Clasico.java
@@ -1,2 +1,8 @@
 import java.textDecimalFormat;
 import java.util.Scanner;
+
+public static void main(String[]args){
+    Scanner lector = new Scanner(System.in);
+    double n,le;
+    double cantidad,to;
+}
